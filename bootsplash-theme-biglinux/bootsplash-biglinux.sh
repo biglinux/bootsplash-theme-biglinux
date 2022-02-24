ae549185f247c8d1821c033bfe2d057ce9e3ef86
BarnabediKartola <barnabedikartola@gmail.com>
2022-02-23 21:40:33 -0300

    att do Rafael Ruscher

--- bootsplash-theme-biglinux/bootsplash-biglinux.sh
+++ bootsplash-theme-biglinux/bootsplash-biglinux.sh
@@ -6,26 +6,17 @@
 # This needs ImageMagick for the 'convert' and 'identify' tools.
 #
 
-LOGO=biglinux.png
-LOGO_WIDTH=$(identify $LOGO | cut -d " " -f 3 | cut -d x -f 1)
-LOGO_HEIGHT=$(identify $LOGO | cut -d " " -f 3 | cut -d x -f 2)
+LOGO=logo.gif
 
-THROBBER=spinner.gif
-THROBBER_WIDTH=$(identify $THROBBER | head -1 | cut -d " " -f 3 | \
+LOGO_WIDTH=$(identify $LOGO | head -1 | cut -d " " -f 3 | \
 						cut -d x -f 1)
-THROBBER_HEIGHT=$(identify $THROBBER | head -1 | cut -d " " -f 3 | \
-						 cut -d x -f 2)
+LOGO_HEIGHT=$(identify $LOGO | head -1 | cut -d " " -f 3 | \
+						cut -d x -f 2)
 
 convert -alpha remove \
 	-background "#000000" \
 	$LOGO \
-	logo.rgb
-
-convert -alpha remove \
-	-background "#000000" \
-	$THROBBER \
-	throbber%02d.rgb
-
+	logo%02d.rgb
 
 #make clean
 #make bootsplash-packer
@@ -36,61 +27,53 @@ convert -alpha remove \
 	--bg_red 0x00 \
 	--bg_green 0x00 \
 	--bg_blue 0x00 \
-	--frame_ms 48 \
+	--frame_ms 100 \
 	--picture \
 	--pic_width $LOGO_WIDTH \
 	--pic_height $LOGO_HEIGHT \
-	--pic_position 0x01 \
-	--pic_position_offset 200 \
-	--blob logo.rgb \
-	--picture \
-	--pic_width $THROBBER_WIDTH \
-	--pic_height $THROBBER_HEIGHT \
-	--pic_position 0x05 \
-	--pic_position_offset 200 \
+	--pic_position 0 \
 	--pic_anim_type 1 \
-	--pic_anim_loop 0 \
-	--blob throbber00.rgb \
-	--blob throbber01.rgb \
-	--blob throbber02.rgb \
-	--blob throbber03.rgb \
-	--blob throbber04.rgb \
-	--blob throbber05.rgb \
-	--blob throbber06.rgb \
-	--blob throbber07.rgb \
-	--blob throbber08.rgb \
-	--blob throbber09.rgb \
-	--blob throbber10.rgb \
-	--blob throbber11.rgb \
-	--blob throbber12.rgb \
-	--blob throbber13.rgb \
-	--blob throbber14.rgb \
-	--blob throbber15.rgb \
-	--blob throbber16.rgb \
-	--blob throbber17.rgb \
-	--blob throbber18.rgb \
-	--blob throbber19.rgb \
-	--blob throbber20.rgb \
-	--blob throbber21.rgb \
-	--blob throbber22.rgb \
-	--blob throbber23.rgb \
-	--blob throbber24.rgb \
-	--blob throbber25.rgb \
-	--blob throbber26.rgb \
-	--blob throbber27.rgb \
-	--blob throbber28.rgb \
-	--blob throbber29.rgb \
-	--blob throbber30.rgb \
-	--blob throbber31.rgb \
-	--blob throbber32.rgb \
-	--blob throbber33.rgb \
-	--blob throbber34.rgb \
-	--blob throbber35.rgb \
-	--blob throbber36.rgb \
-	--blob throbber37.rgb \
-	--blob throbber38.rgb \
-	--blob throbber39.rgb \
-	--blob throbber40.rgb \
-	bootsplash-biglinux
+	--pic_anim_loop 1 \
+	--blob logo00.rgb \
+	--blob logo01.rgb \
+	--blob logo02.rgb \
+	--blob logo03.rgb \
+	--blob logo04.rgb \
+	--blob logo05.rgb \
+	--blob logo06.rgb \
+	--blob logo07.rgb \
+	--blob logo08.rgb \
+	--blob logo09.rgb \
+	--blob logo10.rgb \
+	--blob logo11.rgb \
+	--blob logo12.rgb \
+	--blob logo13.rgb \
+	--blob logo14.rgb \
+	--blob logo15.rgb \
+	--blob logo16.rgb \
+	--blob logo17.rgb \
+	--blob logo18.rgb \
+	--blob logo19.rgb \
+	--blob logo20.rgb \
+	--blob logo21.rgb \
+	--blob logo22.rgb \
+	--blob logo23.rgb \
+	--blob logo24.rgb \
+	--blob logo25.rgb \
+	--blob logo26.rgb \
+	--blob logo27.rgb \
+	--blob logo28.rgb \
+	--blob logo29.rgb \
+	--blob logo30.rgb \
+	--blob logo31.rgb \
+	--blob logo32.rgb \
+	--blob logo33.rgb \
+	--blob logo34.rgb \
+	--blob logo35.rgb \
+	--blob logo36.rgb \
+	--blob logo37.rgb \
+	--blob logo38.rgb \
+	--blob logo39.rgb \
+	bootsplash-biglinux-globo
 
 rm *.rgb
